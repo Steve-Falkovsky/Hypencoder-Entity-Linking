ba66ea61592d528125a1c95e83b79a73edb62203
Steve Falkovsky <37025050+WereWolf232@users.noreply.github.com>
2025-10-18 21:53:23 +0100

    added data extraction from MeSH2015

--- my_stuff/extract_data.py
+++ my_stuff/extract_data.py
@@ -1,5 +1,7 @@
 import gzip
+import random
 import bioc
+import json
 
 # apparently .gz and .tar.gz are different things
 # this is a tar archive
@@ -12,9 +14,24 @@ with gzip.open(source) as file:
     print(f"{file.name=}")
     data = file.read().decode('utf-8')
     collection = bioc.biocxml.loads(data)
-    for document in collection.documents[:5]:
+    for document in collection.documents[:5]: # get first 5 documents
         print(f"{document.id=}")
         for passage in document.passages:
             print(f"{passage.text=}")
             for anno in passage.annotations:
                 print(f"{anno=}")
+                
+                
+                
+mesh = "processed_sources/mesh2015.json.gz"
+
+with gzip.open(mesh, 'rt', encoding='utf-8') as file:
+    print(f"{file.name=}")
+    data = json.load(file)
+
+    if isinstance(data, list):  
+        print(f"Number of items: {len(data)}")
+        
+        # Print random items for inspection
+        for item in random.sample(data, 15):
+            print(json.dumps(item, indent=2))
