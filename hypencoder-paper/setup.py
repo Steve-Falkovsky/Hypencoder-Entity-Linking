4d2fb89c3f2ec05af0e54b93a62ce2ddf028db0d
Steve Falkovsky <37025050+WereWolf232@users.noreply.github.com>
2026-01-02 11:18:15 +0000

    update requirements.txt

--- hypencoder-paper/setup.py
+++ hypencoder-paper/setup.py
@@ -5,15 +5,4 @@ setuptools.setup(
     version="0.0.1",
     packages=["hypencoder_cb"],
     python_requires=">=3.10",
-    install_requires=[
-        'transformers',
-        'tqdm',
-        'more_itertools',
-        'scikit-learn',
-        'torch',
-        'datasets',
-        'fire',
-        'omegaconf',
-        'jsonlines',
-    ],
 )
